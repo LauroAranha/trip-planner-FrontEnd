e43f829aac7a17335407cf30967206a67fac8afd
Joel <joelnunes974@gmail.com>
2023-03-07 15:13:06 -0300

    implementing useState hook on login screeen

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,22 +1,34 @@
 //import ui from '../../firebase/config';
 
 import styles from './login-module.css';
+import { useState } from 'react';
 
-const Login = () => {
+const Login = () => { 
+  const [values, setValues] = useState({
+    email: "",
+    password: ""
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(values)
+
+}
+
   return (
     <div className="login__form">
       <div className="container">
         <h1>Entrar</h1>
         <p>Hello!</p>
-        <form onSubmit="">
+        <form onSubmit ={handleSubmit}>
           <label>
             <input
               type="email"
               name="email"
               required
               placeholder="Enter your email"
-              onChange=""
-              value=""
+              value={values.email}
+              onChange = {(e) => setValues({...values, email: e.target.value})}
+           
             />
           </label>
           {'\n'}
@@ -26,12 +38,11 @@ const Login = () => {
               name="password"
               required
               placeholder="Enter your password"
-              onChange=""
-              value=""
+              value={values.password}
+              onChange = {(e) => setValues({...values, password: e.target.value})}          
             />
           </label>
           {'\n'}
-
           <button className="btn">Enter</button>
         </form>
       </div>

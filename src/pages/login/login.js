ea05522f3dd3a270411e93a4f6b5b01ac19bd8f8
natbrs <natanalmeidasp@gmail.com>
2023-03-13 07:10:27 -0300

    Adding login.js page requirements and styling

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,26 +1,46 @@
-//import ui from '../../firebase/config';
+import './login-module.css';
 
-import styles from './login-module.css';
 import { useState } from 'react';
 import axios from 'axios';
-const Login = () => { 
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEyeSlash, faEye, faUser } from '@fortawesome/free-solid-svg-icons';
+
+document.body.style.overflow = 'hidden';
+
+const Login = () => {
   const [values, setValues] = useState({
-    email: "",
-    password: ""
+    email: '',
+    password: '',
   });
   const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(values)
-    axios.post('http://localhost:3000/user/login ',values
-    )
-}
+    e.preventDefault();
+    console.log(values);
+    axios.post('http://localhost:3000/user/login ', values);
+  };
+
+  const [type, setType] = useState('password');
+  const [icon, setIcon] = useState(faEyeSlash);
+
+  const handleToggle = () => {
+    // For the main password field
+    if (type === 'password') {
+      setIcon(faEye);
+      setType('text');
+    } else {
+      setIcon(faEyeSlash);
+      setType('password');
+    }
+  };
 
   return (
-    <div className="login__form">
-      <div className="container">
-        <h1>Entrar</h1>
-        <p>Hello!</p>
-        <form onSubmit ={handleSubmit}>
+    <div className="page-login">
+      <div className="container-login">
+        <h1>📍 Trip Planner</h1>
+      </div>
+      <h1 className="page-title-login">Login</h1>
+      <div className="page-input-login">
+        <form onSubmit={handleSubmit}>
           <label>
             <input
               type="email"
@@ -28,23 +48,36 @@ const Login = () => {
               required
               placeholder="Enter your email"
               value={values.email}
-              onChange = {(e) => setValues({...values, email: e.target.value})}
-           
+              onChange={(e) => setValues({ ...values, email: e.target.value })}
             />
           </label>
           {'\n'}
           <label>
             <input
-              type="password"
+              type={type}
               name="password"
               required
               placeholder="Enter your password"
               value={values.password}
-              onChange = {(e) => setValues({...values, password: e.target.value})}          
+              onChange={(e) =>
+                setValues({ ...values, password: e.target.value })
+              }
             />
+            <span onClick={handleToggle}>
+              <FontAwesomeIcon icon={icon} />
+            </span>
+            <div className="page-link-login">
+              <a href="" className="name-link">
+                Forgot your password?
+              </a>
+            </div>
           </label>
           {'\n'}
-          <button className="btn">Enter</button>
+          <button className="login-btn">Enter</button>
+          <div className="create-account">
+            <span>Don't have an account?</span>
+            <Link to="/register" className="link-login" >Sign up now</Link>
+          </div>
         </form>
       </div>
     </div>

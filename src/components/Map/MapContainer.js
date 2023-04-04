76812f08d4876367d09c9572be55e6d077c2cee6
Lauro Aranha <84295347+LauroAranha@users.noreply.github.com>
2023-04-04 19:10:20 -0300

    Remove previous google maps package components

--- src/components/Map/MapContainer.js
+++ src/components/Map/MapContainer.js
@@ -1,42 +1,30 @@
-import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import './MapContainer-module.css';
 
-import styles from './MapContainer-module.css';
+import { Loader } from '@googlemaps/js-api-loader';
 
-class MapContainer extends Component {
-  render() {
-    const mapStyles = {
-      display: 'flex',
-      width: '60%',
-      height: '40%',
-      borderRadius: '30px',
-      padding: '0.5%',
-      border: '16px solid #42B0FF',
-      marginTop: '20px',
-    };
+const MapContainer = () => {
+  const mapStyles = {
+    display: 'flex',
+    width: '60%',
+    height: '40%',
+    borderRadius: '30px',
+    padding: '0.5%',
+    border: '16px solid #42B0FF',
+    marginTop: '20px',
+  };
 
-    return (
-      <div className="map-container">
-        <ul className="map-options">
-          <li>Restaurants</li>
-          <li>Parties</li>
-          <li>Ballads</li>
-          <li>Stores</li>
-          <li>Malls</li>
-          <li>Open 24hrs</li>
-        </ul>
-        <Map
-          className="map"
-          google={this.props.google}
-          zoom={8}
-          style={mapStyles}
-          initialCenter={{ lat: -23.5489, lng: -46.6388 }}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="map-container">
+      <ul className="map-options">
+        <li>Restaurants</li>
+        <li>Parties</li>
+        <li>Ballads</li>
+        <li>Stores</li>
+        <li>Malls</li>
+        <li>Open 24hrs</li>
+      </ul>
+    </div>
+  );
+};
 
-export default GoogleApiWrapper({
-  apiKey: '',
-})(MapContainer);
+export default MapContainer;

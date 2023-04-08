932371c10d7cfefb30ad1724246692c49182031b
Lauro Aranha <84295347+LauroAranha@users.noreply.github.com>
2023-04-08 18:09:52 -0300

    Remove remaining Delete references

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
 import Home from './pages/home/home';
 import Login from './pages/login/login';
 import Register from './pages/register/register';
-import Delete from './pages/delete/delete';
 import Edit from './pages/config/edit';
 
 import Roteiro from './pages/roteiro/roteiro';
@@ -16,50 +15,41 @@ import Footer from './components/Footer/Footer';
 import MapContainer from './components/Map/MapContainer';
 
 const App = () => {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <div>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/roteiro" element={<Roteiro />} />
-            <Route
-              path="/home"
-              element={
-                <>
-                  <Navbar />
-                  <div className="container">
-                    <Home />
-                    <MapContainer />
-                  </div>
-                </>
-              }
-            />
-            <Route
-              path="/delete"
-              element={
-                <>
-                  <Navbar />
-                  <Delete />
-                </>
-              }
-            />
-            <Route
-              path="/edit"
-              element={
-                <>
-                  <Navbar />
-                  <Edit />
-                </>
-              }
-            />
-          </Routes>
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <div>
+                    <Routes>
+                        <Route path="/" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                        <Route path="/roteiro" element={<Roteiro />} />
+                        <Route
+                            path="/home"
+                            element={
+                                <>
+                                    <Navbar />
+                                    <div className="container">
+                                        <Home />
+                                        <MapContainer />
+                                    </div>
+                                </>
+                            }
+                        />
+                        <Route
+                            path="/edit"
+                            element={
+                                <>
+                                    <Navbar />
+                                    <Edit />
+                                </>
+                            }
+                        />
+                    </Routes>
+                </div>
+                <Footer />
+            </BrowserRouter>
         </div>
-        <Footer />
-      </BrowserRouter>
-    </div>
-  );
+    );
 };
 
 export default App;

74a067f7d2d87880ebb6aac86be09fe5c9cc5daa
Lauro Aranha <84295347+LauroAranha@users.noreply.github.com>
2023-04-07 22:32:08 -0300

    Get travel itinerary list from firebase

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,50 +1,41 @@
-// CSS
 import './home-module.css';
 
-import image1 from './img/praia-grande.png';
-import image2 from './img/passeio-ibm.png';
-import image3 from './img/visita-fatec.png';
-import image4 from './img/parque-ibirapuera.png';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import TravelSquare from '../../components/Travel-component/TravelSquare';
 
 const Home = () => {
+  const [travelList, setTravelList] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/travel/get/all').then((res) => {
+      const { data } = res;
+      setTravelList(data);
+      setIsLoading(false);
+    });
+  }, []);
+
   return (
     <div className="mainContainer">
       <h1 className="square-title">Recommended Road Maps</h1>
       <div className="squares-container">
-        <div className="square">
-          <img src={image1} alt="Imagem" />
-          <h2>Day trip to Praia Grande</h2>
-          <p>
-            Round trip from SP - Praia Grande, passing by some beaches and local
-            restaurants...
-          </p>
-        </div>
-        <div className="square">
-          <img src={image2} alt="Imagem" />
-          <h2>IBM Ride</h2>
-          <p>
-            Itinerary including trip to IBM Tutoia and Ibirapuera, departing
-            from Paraíso subway station.
-          </p>
-        </div>
-        <div className="square">
-          <img src={image3} alt="Imagem" />
-          <h2>Visit FATEC</h2>
-          <p>
-            Itinerary to get to know Fatec in São Paulo, located near the
-            Tiradentes metro station
-          </p>
-        </div>
-        <div className="square">
-          <img src={image4} alt="Imagem" />
-          <h2>Meet Ibirapuera</h2>
-          <p>
-            Get to know this great park in São Paulo, with large leisure areas
-            and walks to relax.
-          </p>
-        </div>
+        {isLoading ? (
+          <p>carregando</p>
+        ) : (
+          travelList &&
+          travelList.map((object) => {
+            return (
+              <TravelSquare
+                image={object.image}
+                title={object.title}
+                description={object.description}
+              />
+            );
+          })
+        )}
       </div>
-      <h1 className="map-title">Nearby attractions</h1>
     </div>
   );
 };

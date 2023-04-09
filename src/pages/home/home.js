d2eaf52f0317eeea8d95e6fad44a074693d7b82b
Lauro Aranha <84295347+LauroAranha@users.noreply.github.com>
2023-04-09 02:42:59 -0300

    Query recommended trips based on rating

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -6,39 +6,42 @@ import axios from 'axios';
 import TravelSquare from '../../components/Travel-component/TravelSquare';
 
 const Home = () => {
-  const [travelList, setTravelList] = useState();
-  const [isLoading, setIsLoading] = useState(true);
+    const [travelList, setTravelList] = useState();
+    const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    axios.get('http://localhost:3001/travel/get/all').then((res) => {
-      const { data } = res;
-      setTravelList(data);
-      setIsLoading(false);
-    });
-  }, []);
+    useEffect(() => {
+        axios
+            .get('http://localhost:3001/travel/recommendedTravels')
+            .then((res) => {
+                const responseData = res.data.data;
+                console.log(responseData);
+                setTravelList(responseData);
+                setIsLoading(false);
+            });
+    }, []);
 
-  return (
-    <div className="mainContainer">
-      <h1 className="square-title">Recommended Road Maps</h1>
-      <div className="squares-container">
-        {isLoading ? (
-          <p>carregando</p>
-        ) : (
-          travelList &&
-          travelList.map((object) => {
-            return (
-              <TravelSquare
-                image={object.image}
-                title={object.title}
-                description={object.description}
-              />
-            );
-          })
-        )}
-      </div>
-      <h1 className="square-title">Nearby Attractions</h1>
-    </div>
-  );
+    return (
+        <div className="mainContainer">
+            <h1 className="square-title">Recommended Road Maps</h1>
+            <div className="squares-container">
+                {isLoading ? (
+                    <p>carregando</p>
+                ) : (
+                    travelList &&
+                    travelList.map((object) => {
+                        return (
+                            <TravelSquare
+                                image={object.image}
+                                title={object.title}
+                                description={object.description}
+                            />
+                        );
+                    })
+                )}
+            </div>
+            <h1 className="square-title">Nearby Attractions</h1>
+        </div>
+    );
 };
 
 export default Home;

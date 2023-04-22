2e62264d0ad36463a3b7168b55efdfde8be83c14
Lauro Aranha <84295347+LauroAranha@users.noreply.github.com>
2023-04-21 22:06:29 -0300

    Roadmap page changes

--- src/pages/roadmaps-details/roadmap-details.js
+++ src/pages/roadmaps-details/roadmap-details.js
@@ -4,36 +4,89 @@ import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBan, faCheck, faDog } from '@fortawesome/free-solid-svg-icons';
 
 const RoadmapDetails = () => {
-  const { roadmapId } = useParams();
-  const [roadmapDetails, setRoadmapDetails] = useState('');
-  const [isLoading, setIsLoading] = useState(true);
+    const { roadmapId } = useParams();
+    const [roadmapDetails, setRoadmapDetails] = useState('');
+    const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    axios.get(`roadmap/get/${roadmapId}`).then((res) => {
-      const responseData = res.data.data;
-      console.log(responseData);
-      setRoadmapDetails(responseData);
-      setIsLoading(false);
-    });
-  }, []);
-  const array = roadmapDetails.paradasRecomendadas;
+    let countParadas = 0;
 
-  return (
-    <div className="roadmapBox">
-      <p className="title">{roadmapDetails.title}</p>
-      <img src={roadmapDetails.image} alt="Imagem" className="image" />
-      <p>{roadmapDetails.description}</p>
-      <p>{roadmapDetails.custoMedio}</p>
-      <p>{roadmapDetails.cidadeRoteiro}</p>
-      <div className="paradas "></div>
-      <p>{roadmapDetails.pontoInicial}</p>
-      <p>{roadmapDetails.pontoFinal}</p>
-      <p>{roadmapDetails.petsOk}</p>
-      <p>{roadmapDetails.criancaOk}</p>
-    </div>
-  );
+    useEffect(() => {
+        axios.get(`roadmap/get/${roadmapId}`).then((res) => {
+            const responseData = res.data.data;
+            setRoadmapDetails(responseData);
+            setIsLoading(false);
+        });
+    }, []);
+
+    return (
+        <div className="roadmapBox">
+            <h1 className="title">Título</h1>
+            <p className="title">{roadmapDetails.title}</p>
+
+            <img src={roadmapDetails.image} alt="Imagem" className="image" />
+
+            <h2 className="roadmapInformation">Descrição</h2>
+            <p className="roadmapInformation">{roadmapDetails.description}</p>
+
+            <h2 className="roadmapInformation">Custo médio por pessoa</h2>
+            <p className="roadmapInformation">R${roadmapDetails.custoMedio}</p>
+
+            <h2 className="roadmapInformation">Cidade do roteiro</h2>
+            <p className="roadmapInformation">{roadmapDetails.cidadeRoteiro}</p>
+
+            <h2 className="roadmapInformation">Ponto de parada recomendados</h2>
+            {roadmapDetails &&
+                roadmapDetails.paradasRecomendadas.map((object) => {
+                    countParadas++;
+                    return (
+                        <p className="roadmapInformation">
+                            Parada {countParadas}: {object}
+                        </p>
+                    );
+                })}
+
+            <h2 className="roadmapInformation">Ponto de partida</h2>
+            <p className="roadmapInformation">{roadmapDetails.pontoInicial}</p>
+
+            <h2 className="roadmapInformation">Ponto final</h2>
+            <p className="roadmapInformation">{roadmapDetails.pontoFinal}</p>
+
+            {roadmapDetails.petsOk ? (
+                <>
+                    <h2 className="roadmapInformation">Pet friendly</h2>
+                    <FontAwesomeIcon icon={faDog} className="nav-icon" />
+                </>
+            ) : (
+                <>
+                    <h2 className="roadmapInformation">
+                        {' '}
+                        Não aconselhável a pets
+                    </h2>
+                    <FontAwesomeIcon icon={faBan} className="nav-icon" />
+                </>
+            )}
+
+            {roadmapDetails.criancaOk ? (
+                <>
+                    <h2 className="roadmapInformation">
+                        Aconselhável a menores de idade
+                    </h2>
+                    <FontAwesomeIcon icon={faCheck} className="nav-icon" />
+                </>
+            ) : (
+                <>
+                    <h2 className="roadmapInformation">
+                        Não aconselhável a menores de idade
+                    </h2>
+                    <FontAwesomeIcon icon={faBan} className="nav-icon" />
+                </>
+            )}
+        </div>
+    );
 };
 
 export default RoadmapDetails;

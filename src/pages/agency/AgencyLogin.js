c50327c7d85859d72c756be75d439f9febeb8b3f
Lauro Aranha <84295347+LauroAranha@users.noreply.github.com>
2023-05-07 23:21:06 -0300

    Add agency login page

---
+++ src/pages/agency/AgencyLogin.js
@@ -0,0 +1,127 @@
+import './AgencyLogin.css';
+
+import { useState } from 'react';
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEyeSlash, faEye } from '@fortawesome/free-solid-svg-icons';
+import ErrorComponent from '../../components/SystemMessagesComponents/ErrorComponent/ErrorComponent';
+
+const AgencyLogin = () => {
+    const navigate = useNavigate();
+    const [errorMessage, setErrorMessage] = useState('');
+    const [type, setType] = useState('password');
+    const [icon, setIcon] = useState(faEyeSlash);
+    const [values, setValues] = useState([]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('user/login', values);
+            if (response.status === 200) {
+                sessionStorage.setItem('userToken', response.data.data.token);
+                sessionStorage.setItem(
+                    'currentUserInfo',
+                    JSON.stringify(response.data.data.currentUserInfo)
+                );
+                navigate('/home');
+            }
+        } catch (error) {
+            setErrorMessage('Invalid username or password');
+        }
+    };
+
+    const handleToggle = () => {
+        if (type === 'password') {
+            setIcon(faEye);
+            setType('text');
+        } else {
+            setIcon(faEyeSlash);
+            setType('password');
+        }
+    };
+
+    return (
+        <div className="page-login">
+            <div className="container-login">
+                <h1>📍 Trip Planner</h1>
+            </div>
+            <div className="container-input-login">
+                <form className="login-form" onSubmit={handleSubmit}>
+                    <div className="login-input-fields">
+                        <h1 className="page-title-login">Login</h1>
+                        <label className="login-form-label">
+                            Insira seu e-mail
+                        </label>
+                        <input
+                            type="email"
+                            name="email"
+                            required
+                            placeholder="Enter your email"
+                            value={values.email}
+                            onChange={(e) =>
+                                setValues({ ...values, email: e.target.value })
+                            }
+                        />
+                        <label className="login-form-label">Insira sua senha</label>
+                        <div className="inputWithSpan">
+                            <input
+                                type={type}
+                                name="password"
+                                required
+                                placeholder="Enter your password"
+                                value={values.currentPassword}
+                                onChange={(e) =>
+                                    setValues({
+                                        ...values,
+                                        currentPassword: e.target.value,
+                                    })
+                                }
+                            />
+                            <span onClick={handleToggle} className="icon-eye">
+                                <FontAwesomeIcon icon={icon} />
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="page-link-login">
+                        <a href="" className="name-link">
+                            Forgot your password?
+                        </a>
+                    </div>
+                    <button type="submit" className="login-btn">
+                        Enter
+                    </button>
+
+                    {errorMessage && <ErrorComponent title={"Sign in error!"} message={errorMessage} width={"15vw"} height={"10vw"} margin={"1vw auto"} />}
+
+                    <div className="create-account">
+                        <span className="login-sign-in">
+                            I'm already registered
+                        </span>
+                        <Link to="/" className="link-login">
+                            Sign in here
+                        </Link>
+                    </div>
+                    <div className="create-account">
+                        <span className="login-sign-in">
+                            Tourism Agency?
+                        </span>
+                        <Link to="/register-agency" className="link-login">
+                            Sign up here
+                        </Link>
+                        <span className="login-sign-in">
+                            or
+                        </span>
+                        <Link to="/login-agency" className="link-login">
+                            Sign in here
+                        </Link>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default AgencyLogin;
+

c92c26254a63aaff74e9a1f315576d28322e85d2
Natan Almeida Ramos Rezende <83055901+natbrs@users.noreply.github.com>
2023-05-30 19:38:48 -0300

    Update MapContainer.js

--- src/components/Map/MapContainer.js
+++ src/components/Map/MapContainer.js
@@ -1,73 +1,85 @@
 import { useEffect } from 'react';
-import './MapContainer-module.css';
+import './MapContainer.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faUtensils,
-  faBirthdayCake,
-  faGlassCheers,
-  faShoppingBag,
-  faShoppingBasket,
-  faClock,
+    faUtensils,
+    faBirthdayCake,
+    faGlassCheers,
+    faShoppingBag,
+    faShoppingBasket,
+    faClock,
 } from '@fortawesome/free-solid-svg-icons';
 
 import {
-  initGoogleMapApiScript,
-  initMapWithAutocompleteField,
+    initGoogleMapApiScript,
+    initMapWithAutocompleteField,
 } from '../utils/mapFunctions';
 
 const MapContainer = () => {
-  useEffect(() => {
-    window.initMapWithAutocompleteField = initMapWithAutocompleteField;
-    const scriptReturned = initGoogleMapApiScript(
-      initMapWithAutocompleteField.name
+    useEffect(() => {
+        window.initMapWithAutocompleteField = initMapWithAutocompleteField;
+        const scriptReturned = initGoogleMapApiScript(
+            initMapWithAutocompleteField.name
+        );
+
+        return () => {
+            // Clean up by removing the script and window property
+            document.body.removeChild(scriptReturned);
+            delete window.scriptReturned;
+        };
+    }, []);
+
+    return (
+        <div className="map-container">
+            <input
+                id="pac-input"
+                className="controls"
+                type="text"
+                placeholder="Search For Nearby Attractions"
+            />
+            {/* TODO We have to put it in a separate component to be able to reuse mapComponent without the options */}
+            <ul className="map-options">
+                <li>
+                    <FontAwesomeIcon icon={faUtensils} className="map-icon" />
+                    Restaurants
+                </li>
+                <li>
+                    <FontAwesomeIcon
+                        icon={faBirthdayCake}
+                        className="map-icon"
+                    />
+                    Parties
+                </li>
+                <li>
+                    <FontAwesomeIcon
+                        icon={faGlassCheers}
+                        className="map-icon"
+                    />
+                    Ballads
+                </li>
+                <li>
+                    <FontAwesomeIcon
+                        icon={faShoppingBag}
+                        className="map-icon"
+                    />
+                    Stores
+                </li>
+                <li>
+                    <FontAwesomeIcon
+                        icon={faShoppingBasket}
+                        className="map-icon"
+                    />
+                    Malls
+                </li>
+                <li>
+                    <FontAwesomeIcon icon={faClock} className="map-icon" />
+                    Open 24hrs
+                </li>
+            </ul>
+            <div id="map" className="map-styles" />
+        </div>
     );
-
-    return () => {
-      // Clean up by removing the script and window property
-      document.body.removeChild(scriptReturned);
-      delete window.scriptReturned;
-    };
-  }, []);
-
-  return (
-    <div className="map-container">
-      <input
-        id="pac-input"
-        className="controls"
-        type="text"
-        placeholder="Search For Nearby Attractions"
-      />
-      {/* TODO We have to put it in a separate component to be able to reuse mapComponent without the options */}
-      <ul className="map-options">
-        <li>
-          <FontAwesomeIcon icon={faUtensils} className="map-icon" />
-          Restaurants
-        </li>
-        <li>
-          <FontAwesomeIcon icon={faBirthdayCake} className="map-icon" />
-          Parties
-        </li>
-        <li>
-          <FontAwesomeIcon icon={faGlassCheers} className="map-icon" />
-          Ballads
-        </li>
-        <li>
-          <FontAwesomeIcon icon={faShoppingBag} className="map-icon" />
-          Stores
-        </li>
-        <li>
-          <FontAwesomeIcon icon={faShoppingBasket} className="map-icon" />
-          Malls
-        </li>
-        <li>
-          <FontAwesomeIcon icon={faClock} className="map-icon" />
-          Open 24hrs
-        </li>
-      </ul>
-      <div id="map" className="map-styles" />
-    </div>
-  );
 };
 
 export default MapContainer;

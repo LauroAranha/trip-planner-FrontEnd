4b5e962713b6b62d30c40b5006005cee25b43256
Natan Almeida Ramos Rezende <83055901+natbrs@users.noreply.github.com>
2023-06-13 12:59:53 -0300

    Update RoadmapSquare.js

--- src/components/Roadmap-component/RoadmapSquare.js
+++ src/components/Roadmap-component/RoadmapSquare.js
@@ -5,16 +5,16 @@ const RoadmapSquare = (props) => {
 
     const handleLongDescriptions = (description) => {
         if (description.length > 150) {
-            return `${description.slice(0, 150)}...`
+            return `${description.slice(0, 150)}...`;
         }
-        return description
-    }
+        return description;
+    };
     return (
-        <div className="roadmap-card">
-            <img src={image} alt="Imagem" />
-            <div className='roadmap-card-information'>
-                <h2>{title}</h2>
-                <p>{handleLongDescriptions(description)}</p>
+        <div className="personal-roadmap-content card" style={{width: '20rem'}}>
+            <img src={image} alt="Imagem" className="personal-roadmap-image img-fluid" style={{ width: '100%', height: '200px' }} />
+            <div className="roadmap-card-information card-body">
+                <h2 className="card-title">{title}</h2>
+                <p className="card-text">{handleLongDescriptions(description)}</p>
             </div>
         </div>
     );
